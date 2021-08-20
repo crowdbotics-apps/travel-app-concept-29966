3bf40148ba58b00245582229ece30165a842a616
Crowdbotics <team@crowdbotics.com>
2021-08-20 09:29:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "travel_app_concept_29966Identifier",
+  url: "https://travel_app_concept_29966Identifier.botics.co",
+  api: "https://travel_app_concept_29966Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
